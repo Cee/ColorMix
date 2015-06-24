c260e5d0cce102555c28504aaa8c8bcec3d7a31a
Cee <cee@chu2byo.com>
2015-06-24 14:16:25 +0800

    Code rebase

---
+++ ColorMix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorMix
+//
+//  Created by Cee on 24/06/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColorMix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorMix
+//
+//  Created by Cee on 24/06/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
