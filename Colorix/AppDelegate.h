065dab810bd22e4fb6f99599573278a68f1a2662
Cee <cee@chu2byo.com>
2015-06-24 11:49:56 +0800

    Code init

---
+++ Colorix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Colorix
+//
+//  Created by Cee on 24/06/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Colorix/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Colorix
+//
+//  Created by Cee on 24/06/2015.
+//  Copyright (c) 2015 Cee. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
